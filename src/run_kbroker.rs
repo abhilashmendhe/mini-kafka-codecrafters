4f64e7ca2c67f6f66e89dcbcceb4f3fc5f6f5d3e
abhilashmendhe <abhilashmendhe@gmail.com>
2025-07-23 12:55:26 +0530

    codecrafters submit [skip ci]

--- src/run_kbroker.rs
+++ src/run_kbroker.rs
@@ -11,13 +11,26 @@ pub async fn run_kafka_broker() -> Result<(), KafkaErrors> {
 
         let (mut reader, mut writer) = stream.into_split();
 
-        let mut buffer = [0u8; 1024];
+        // let mut buffer = [0u8; 1024];
 
-        let size = reader.read(&mut buffer).await?;
+        let mut msg_buf = [0u8; 4];
+        let _msg_n = reader.read_exact(&mut msg_buf).await?;
 
-        println!("buffer: {:?}", &buffer[..size]);
+        let mut req_api_key = [0u8; 2];
+        let _req_api_key_n = reader.read_exact(&mut req_api_key).await?;
 
-        writer.write(&[0u8,0,0,0,0,0,0,07]).await?;
+        let mut req_api_ver = [0u8; 2];
+        let _req_api_ver_n = reader.read_exact(&mut req_api_ver).await?;
+
+        let mut corr_id = [0u8; 4];
+        let _corr_id_n = reader.read_exact(&mut corr_id).await?;
+        
+        println!("corr_id(u8): {:?}", &corr_id);
+
+        let mut write_buf = Vec::new();
+        write_buf.extend_from_slice(&[0,0,0,0]);
+        write_buf.extend_from_slice(&corr_id);
+        writer.write(&write_buf).await?;
     }
-    Ok(())
+    // Ok(())
 }
