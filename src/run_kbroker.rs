9fe5b044c8983f239b4d9a242a28d5f4b88f47bd
abhilashmendhe <abhilashmendhe@gmail.com>
2025-07-23 12:44:27 +0530

    codecrafters submit [skip ci]

--- src/run_kbroker.rs
+++ src/run_kbroker.rs
@@ -6,17 +6,18 @@ use crate::kafka_errors::KafkaErrors;
 pub async fn run_kafka_broker() -> Result<(), KafkaErrors> {
 
     let listener = TcpListener::bind("127.0.0.1:9092").await?;
-    
-    let (stream, _sock_addr) = listener.accept().await?;
+    loop {
+        let (stream, _sock_addr) = listener.accept().await?;
 
-    let (mut reader, mut writer) = stream.into_split();
+        let (mut reader, mut writer) = stream.into_split();
 
-    let mut buffer = [0u8; 1024];
+        let mut buffer = [0u8; 1024];
 
-    let size = reader.read(&mut buffer).await?;
+        let size = reader.read(&mut buffer).await?;
 
-    println!("buffer: {:?}", &buffer[..size]);
+        println!("buffer: {:?}", &buffer[..size]);
 
-    writer.write(&[0u8,0,0,0,0,0,0,07]).await?;
+        writer.write(&[0u8,0,0,0,0,0,0,07]).await?;
+    }
     Ok(())
 }
